fa41c1b08625b71cd82f0fbf6b62ba76a47c7bb3
GLOBAL-WORDS <hamadinemoctar0@gmail.com>
2025-10-29 12:21:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
 // app.js
-// Point d’entrée principal du projet Tadaksahak Learning
-// -------------------------------------------------------
+// Initialisation générale du site Tadaksahak Learning
+// ---------------------------------------------------
 
-import { initialiserNavigation } from './modules/navigation.js';
-import { initialiserPartage } from './modules/share.js';
 import { initialiserDictionnaire } from './modules/dictionnaire.js';
 import { initialiserChatbot } from './modules/chatbot.js';
 import { initialiserAudio } from './modules/audio.js';
 import { initialiserPhotos } from './modules/photos.js';
 import { initialiserVideos } from './modules/videos.js';
-import { initialiserLivres } from './modules/livres.js';
 import { initialiserRapports } from './modules/rapports.js';
+import { initialiserLivres } from './modules/livres.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c🚀 Tadaksahak Learning initialisé', 'color:#00bcd4;font-weight:bold;');
+  console.log('🚀 Tadaksahak Learning modules init');
 
-  // Initialisation des modules
-  initialiserNavigation();
-  initialiserPartage();
   initialiserDictionnaire();
   initialiserChatbot();
   initialiserAudio();
   initialiserPhotos();
   initialiserVideos();
-  initialiserLivres();
   initialiserRapports();
+  initialiserLivres();
 });
